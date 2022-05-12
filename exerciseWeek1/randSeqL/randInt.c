4597165c1a048b664e6e8b051021195fc1418c09
runyx1325 <lucaburst11@gmail.com>
2022-05-12 22:56:41 +0200

    Disable warnings and deleted some code

--- exerciseWeek1/randSeqL/randInt.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char* argv[]) {
-	for (int i = 0; i < atoi(argv[1]); i++) {
-		int value = rand() % atoi(argv[2]);
-		printf("%d\n", value);
-	}
-}
